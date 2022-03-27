a7731bf825e1644cfb5a42311257b95bc8d42d8c
Restyled.io <commits@restyled.io>
2022-03-27 17:06:36 +0000

    Restyled by shfmt

--- doc/build.sh
+++ doc/build.sh
@@ -4,10 +4,10 @@ platform=zhihu
 fn=src/v1-cn.md
 
 md2zhihu \
-    --platform         "$platform" \
-    --code-width       600 \
-    --refs             src/refs.yml \
-    --output-dir       ./built \
-    --asset-output-dir ./built/"$platform" \
-    --md-output        ./built/"$platform"/ \
-    "$fn"
+  --platform "$platform" \
+  --code-width 600 \
+  --refs src/refs.yml \
+  --output-dir ./built \
+  --asset-output-dir ./built/"$platform" \
+  --md-output ./built/"$platform"/ \
+  "$fn"
